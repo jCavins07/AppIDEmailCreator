4e8e27e68541f9d567171548c99e4ed5bfdd2897
Jasper Cavins <jasperCavins97@gmail.com>
2021-02-22 15:59:15 -0800

    More email string added
    Added more strings and ate pass number as an input

--- UIApp/CreateTheEmail.py
+++ UIApp/CreateTheEmail.py
@@ -12,11 +12,21 @@ partNumber = input("What is the part number: ")
 print("The partnumber is " + str(partNumber))
 prevFirmware = input("What was the previous firmware: ")
 print("The previous firmware was " + str(prevFirmware)) 
+atePass = input("What is the ATE pass number: ")
+print("The ate pass number is " + str(atePass))
+
 
 
 partOne = "All,\n Here is the test report for the : " 
 
 fullString = partOne + device + " " + radio + " AppID: " +  appID + " Firmware: " + firmware
+
+partTwo = "\n\n\nWhat was tested for this release:\n ATE PASS Test ID: " + str(atePass)
+
+fullString += partTwo 
+partThree = "\nP/N: " + str(partNumber)
+
+fullString += partThree
 
 print("************************************")
 print("Full text")
