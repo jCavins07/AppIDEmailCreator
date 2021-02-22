36865aa42c84998921b80dfe216096f3662bc53d
Jasper Cavins <jasperCavins97@gmail.com>
2021-02-22 15:41:42 -0800

    More inputs
    Got more inputs for filling out the email out the App ID.

--- UIApp/CreateTheEmail.py
+++ UIApp/CreateTheEmail.py
@@ -7,7 +7,11 @@ print("The radio is " + str(radio))
 appID = input("What App ID are you working with: ")
 print("You are working with " + str(appID))
 firmware = input("What is the firmware: ")
-print("The firmware you put is: " str(firmware))
+print("The firmware you put is " + str(firmware))
+partNumber = input("What is the part number: ")
+print("The partnumber is " + str(partNumber))
+prevFirmware = input("What was the previous firmware: ")
+print("The previous firmware was " + str(prevFirmware)) 
 
 
 partOne = "All,\n Here is the test report for the : " 
